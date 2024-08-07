72f243da1a4c31cb73a40648986b6942d7433010
vmantrip762000 <mvsadi76@gmail.com>
2024-08-07 13:34:30 -0500

    Changing the first commit

---
+++ src/main/java/io/aiven/spring/mysql/demo/MainController.java
@@ -0,0 +1,34 @@
+package io.aiven.spring.mysql.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+@RequestMapping(path = "/demo")
+public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody User addNewUser(@RequestParam String name,
+                                         @RequestParam String email){
+
+        User springUser = new User();
+        springUser.setName(name);
+        springUser.setEmail(email);
+        userRepository.save(springUser);
+        return springUser;
+
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+
+        return userRepository.findAll();
+        //lists users in xml or json files
+
+    }
+
+}
